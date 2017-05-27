06741dd11ed30fa120d075f43439d362d8ea1157
qiuxinlin <qiuxnlin@163.com>
2017-05-27 15:22:44 +0800

    添加流程编辑器

--- activitiConsumer/src/main/java/com/scxys/activiti/deviceException/controller/DeviceExceptionController.java
+++ activitiConsumer/src/main/java/com/scxys/activiti/deviceException/controller/DeviceExceptionController.java
@@ -1,5 +1,6 @@
 package com.scxys.activiti.deviceException.controller;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -7,20 +8,27 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.Execution;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.scxys.activiti.bean.DeviceException;
 import com.scxys.activiti.deviceException.dao.DeviceExceptionDao;
 import com.scxys.activiti.utils.SessionContext;
+import com.scxys.bean.WorkflowBean;
 
 /**
  * @author 作者:qiuxinlin
@@ -39,6 +47,8 @@ public class DeviceExceptionController {
 	TaskService taskService;
 	@Autowired
 	RepositoryService repositoryService;
+	@Autowired
+	ProcessEngine processEngine;
 
 	@RequestMapping("home")
 	public String home(Model model) {
@@ -68,31 +78,31 @@ public class DeviceExceptionController {
 		return "redirect:/deviceExceptionController/home";
 	}
 
-	@RequestMapping("start")
-	public String start() {
+	@RequestMapping(value = "start/{businessId}")
+	public String start(@PathVariable("businessId") String businessId) {
 		String key = DeviceException.class.getSimpleName();
 		System.out.println("--------------" + key);
-
 		Map<String, Object> variables = new HashMap<String, Object>();
 		variables.put("applyUserId", SessionContext.applyUserId);// 表示惟一用户
-		runtimeService.startProcessInstanceByKey(key, variables);
+		runtimeService.startProcessInstanceByKey(key, businessId, variables);
 		return "redirect:/deviceExceptionController/listTask";
 	}
 
 	@RequestMapping("listTask")
 	public String listTask(Model model) {
 		List<Task> list = taskService.createTaskQuery()//
-				.taskAssignee(SessionContext.applyUserId)// 指定个人任务查询
+				// .taskAssignee(SessionContext.applyUserId)// 指定个人任务查询
 				.orderByTaskCreateTime().asc()// 升序
 				.list();
 		model.addAttribute("list", list);
 		return "deviceException/task";
 	}
-	@RequestMapping(value="findResource/{deploymentId}", method = RequestMethod.GET, produces = "text/xml")
+
+	@RequestMapping(value = "findResource/{deploymentId}", method = RequestMethod.GET, produces = "text/xml")
 	public String findResource(@PathVariable("deploymentId") String deploymentId, HttpServletResponse response)
 			throws IOException {
-		 Deployment deployment = repositoryService.createDeploymentQuery().deploymentId(deploymentId).singleResult();
-		 String resourceName=deployment.getName()+".bpmn20.xml";
+		Deployment deployment = repositoryService.createDeploymentQuery().deploymentId(deploymentId).singleResult();
+		String resourceName = deployment.getName() + ".bpmn20.xml";
 		// 获取资源文件表（act_ge_bytearray）中资源图片输入流InputStream
 		InputStream inputStream = repositoryService.getResourceAsStream(deploymentId, resourceName);
 		// 从response对象获取输出流
@@ -105,31 +115,36 @@ public class DeviceExceptionController {
 		inputStream.close();
 		return null;
 	}
-	/*@RequestMapping(value="/repository/deployments/{deploymentId}/resources/**", method = RequestMethod.GET, produces = "application/json")
-	  public DeploymentResourceResponse getDeploymentResource(@PathVariable("deploymentId") String deploymentId, 
-	      HttpServletRequest request) {
-	    
-	    // Check if deployment exists
-	    Deployment deployment = repositoryService.createDeploymentQuery().deploymentId(deploymentId).singleResult();
-	    if (deployment == null) {
-	      throw new ActivitiObjectNotFoundException("Could not find a deployment with id '" + deploymentId + "'.");
-	    }
-	    
-	    String pathInfo = request.getPathInfo();
-	    String resourceName = pathInfo.replace("/repository/deployments/" + deploymentId + "/resources/", "");
-	    
-	    List<String> resourceList = repositoryService.getDeploymentResourceNames(deploymentId);
-	    
-	    if (resourceList.contains(resourceName)) {
-	      // Build resource representation
-	      DeploymentResourceResponse response = restResponseFactory.createDeploymentResourceResponse(deploymentId, resourceName, 
-	          contentTypeResolver.resolveContentType(resourceName));
-	      return response;
-	      
-	    } else {
-	      // Resource not found in deployment
-	      throw new ActivitiObjectNotFoundException("Could not find a resource with id '" + resourceName
-	              + "' in deployment '" + deploymentId + "'.");
-	    }
-	  }*/
+
+	@RequestMapping("viewTaskForm")
+	public String viewTaskForm(Model model, String taskId) {
+		Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
+		String processInstanceId = task.getProcessInstanceId();
+		ProcessInstance pi = runtimeService.createProcessInstanceQuery()//
+				.processInstanceId(processInstanceId)// 使用流程实例ID查询
+				.singleResult();
+		// 4：使用流程实例对象获取BUSINESS_KEY
+		String buniness_key = pi.getBusinessKey();
+		DeviceException deviceException = deviceExceptionDao.findOne(Long.parseLong(buniness_key));
+		model.addAttribute("dException", deviceException);
+		model.addAttribute("taskId", taskId);
+		return "deviceException/viewform";
+	}
+
+	@RequestMapping("submitTask")
+	public String submitTask(String taskId, String result) {
+		Map<String, Object> variables = new HashMap<>();
+		variables.put("result", result);
+		taskService.complete(taskId, variables);
+		return "redirect:/deviceExceptionController/listTask";
+	}
+
+	@RequestMapping("claim")
+	/** 拾取任务，将组任务分配给个人任务，指定任务的办理人 */
+	public String claim(String taskId, String userId) {
+		// 分配的个人任务（可以是组任务中的成员，也可以是非组任务的成员）
+		taskService.claim(taskId, userId);
+		System.out.println("分配任务成功");
+		return "redirect:/deviceExceptionController/listTask";
+	}
 }
