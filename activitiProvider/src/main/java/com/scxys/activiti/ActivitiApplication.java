97259c3e581c19e77fc408ac406cf368049df23a
qiuxinlin <qiuxnlin@163.com>
2017-06-07 10:42:50 +0800

    添加跨域类CorsConfig

--- activitiProvider/src/main/java/com/scxys/activiti/ActivitiApplication.java
+++ activitiProvider/src/main/java/com/scxys/activiti/ActivitiApplication.java
@@ -4,10 +4,6 @@ package com.scxys.activiti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.scxys.activiti.interceptor.TestInterceptor;
 
 /**
  * Created by pengyingzhi on 2017/3/20.
