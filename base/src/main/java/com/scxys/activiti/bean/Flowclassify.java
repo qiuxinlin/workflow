0a7f5d6fc7d1e69d124ee4f1143f51fb00e10971
qiuxinlin <qiuxnlin@163.com>
2017-07-03 16:52:50 +0800

    add flowclassify

---
+++ base/src/main/java/com/scxys/activiti/bean/Flowclassify.java
@@ -0,0 +1,74 @@
+package com.scxys.activiti.bean;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/** 
+* @author 作者:qiuxinlin 
+* @version 创建时间:2017年7月3日 下午1:40:41 
+* @description 说明:
+*/
+@Entity
+@Table(name="act_flowclassify")
+public class Flowclassify implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	private Long id;//主键ID
+	@Column
+	private String classifyCode;//分类编码
+	@Column
+	private String classifyName;//分类名称
+	@Column
+	private String parentCode;//上级编码
+	@Column
+	private String FullPath;//分类全路径
+	@Column
+	private String Describtion;//分类描述
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getClassifyCode() {
+		return classifyCode;
+	}
+	public void setClassifyCode(String classifyCode) {
+		this.classifyCode = classifyCode;
+	}
+	public String getClassifyName() {
+		return classifyName;
+	}
+	public void setClassifyName(String classifyName) {
+		this.classifyName = classifyName;
+	}
+	public String getParentCode() {
+		return parentCode;
+	}
+	public void setParentCode(String parentCode) {
+		this.parentCode = parentCode;
+	}
+	public String getFullPath() {
+		return FullPath;
+	}
+	public void setFullPath(String fullPath) {
+		FullPath = fullPath;
+	}
+	public String getDescribtion() {
+		return Describtion;
+	}
+	public void setDescribtion(String describtion) {
+		Describtion = describtion;
+	}
+	
+	
+}
+ 
