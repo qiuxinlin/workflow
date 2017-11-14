784c7898d74f225c5cc07612e90fe698a37b1929
qiuxinlin <qiuxnlin@163.com>
2017-11-14 10:00:13 +0800

    增加流程定义和代办任务的带条件查询 去掉暂时不用的页面

--- activitiConsumer/src/main/java/com/scxys/activiti/controller/uiController/UiController.java
+++ activitiConsumer/src/main/java/com/scxys/activiti/controller/uiController/UiController.java
@@ -1,22 +1,21 @@
 package com.scxys.activiti.controller.uiController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 页面controller
  */
 @Controller
-@RequestMapping(value = "activiti")
+@RequestMapping("activiti")
 public class UiController {
 
-    @RequestMapping(value = "/toDiagram")
-    public ModelAndView toDiagram(String name,String description) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("name",name);
-        modelAndView.addObject("description",description);
-        modelAndView.setViewName("redirect:/businessProcessManager/html/diagram.html");
-        return modelAndView;
+    @RequestMapping("/toDiagram")
+    public String toDiagram(String name, String description, Model model) {
+        model.addAttribute("name",name);
+        model.addAttribute("description",description);
+        //modelAndView.setViewName("/businessProcessManager/html/diagram.html");
+        return "/businessProcessManager/html/diagram";
     }
 }
