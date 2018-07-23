b3a076f79defa56b2e136d28c66dba20abbb8af0
qiuxinlin <qiuxnlin@163.com>
2018-07-23 16:50:36 +0800

    代码合并

--- src/main/java/com/scxys/activiti/controller/WorkflowController.java
+++ src/main/java/com/scxys/activiti/controller/WorkflowController.java
@@ -1,10 +1,8 @@
 package com.scxys.activiti.controller;
 
 import com.neoinfo.pojo.CommRes;
-import com.scxys.activiti.bean.commonBean.User;
 import com.scxys.activiti.service.ActDelegationService;
 import com.scxys.activiti.service.WorkflowService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,14 +25,13 @@ public class WorkflowController {
 
     private
     ActDelegationService delegationService;
-    @Autowired User user;
 
     /**
      * @author 作者: qiuxinlin
      * @version 创建时间:2017年7月11日 下午3:16:23
      * @description 说明:全任务委托
      */
-    @RequestMapping(value="/delegateTasks", method=RequestMethod.GET)
+    @RequestMapping(value = "/delegateTasks", method = RequestMethod.GET)
     @ResponseBody
     public String delegateTasks(@RequestParam("id") String id, @RequestParam("startDate") String afterDate, @RequestParam("endDate") String beforeDate, @RequestParam("owner") String assignee, @RequestParam("assignee") String delegateUser) {
         if (afterDate == null || afterDate.isEmpty()) {
@@ -47,40 +44,30 @@ public class WorkflowController {
             return "请选择委托人";
         }
         workflowService.delegateTasks(afterDate, beforeDate, assignee, delegateUser);
-        Long delegationId=Long.parseLong(id);
+        Long delegationId = Long.parseLong(id);
         delegationService.updateStatus(delegationId);
         return "委托成功";
     }
+
     /**
      * 发布流程
      *
      * @return
      * @throws IOException
      */
-    @RequestMapping(value = "/deployment",method = RequestMethod.POST ,produces = "application/json")
+    @RequestMapping(value = "/deployment", method = RequestMethod.POST, produces = "application/json")
     @ResponseBody
-    public CommRes deployment(String name, String diagramData, String svgData, String category ) {
-        if(name==null||name.isEmpty()){
-            return CommRes.errorRes("400","部署名称为空");
+    public CommRes deployment(String name, String diagramData, String svgData, String category) {
+        if (name == null || name.isEmpty()) {
+            return CommRes.errorRes("400", "部署名称为空");
         }
-        if(diagramData==null||diagramData.isEmpty()){
-            return CommRes.errorRes("400","流程图数据为空");
+        if (diagramData == null || diagramData.isEmpty()) {
+            return CommRes.errorRes("400", "流程图数据为空");
         }
-        if(svgData==null||svgData.isEmpty()){
-            return CommRes.errorRes("400","图片数据为空");
+        if (svgData == null || svgData.isEmpty()) {
+            return CommRes.errorRes("400", "图片数据为空");
         }
-        workflowService.deployment(name,diagramData,svgData,category);
+        workflowService.deployment(name, diagramData, svgData, category);
         return CommRes.successRes();
-    }
-    /**
-     * @Author: qiuxinlin
-     * @Dercription: 获取从别的系统登陆过来的用户
-     * @Date: 2017/9/19
-     */
-    @RequestMapping(value = "/login",method = RequestMethod.GET )
-    public String login(String userName){
-        user.setName(userName);
-        System.out.println(user.getName());
-        return "redirect:/businessProcessManager/html/process-list.html";
     }
 }
